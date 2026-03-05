34881aae03d3f0b6ab6a708bf0e9dc86e6b1fe9b
ansalwrk <ansalwrk@gmail.com>
2026-03-05 10:02:51 +0530

    .

--- my-react-app/src/Component1.jsx
+++ my-react-app/src/Component1.jsx
@@ -1,6 +1,10 @@
+import "./Component1.css";
+
 function Component1() {
   return (
     <div className="component-1">
+      <h2>Welcome to Component 1</h2>
+      <button className="contact-btn">Contact</button>
     </div>
   );
 }
