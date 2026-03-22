400f316a6789f272aa62dc3f8bab9417b0b8155f
ansalwrk <ansalwrk@gmail.com>
2026-03-22 21:24:14 +0530

    .

--- my-react-app/src/Screen/Screen4.jsx
+++ my-react-app/src/Screen/Screen4.jsx
@@ -6,6 +6,50 @@ return (
 <>
 
 
+<div style={{
+
+  backgroundColor:"#000000",}}     >       
+  <div class="grid grid cols-1 ml-10">
+           <div style={{ 
+              backgroundColor: "#b3390067", 
+              color: "#ff5100", 
+              padding: "8px 16px",  
+              borderRadius: "20px", 
+              fontWeight: "bold", 
+              fontSize:"13px",
+              display: "inline-block", 
+              marginBottom: "15px",  width:"6%"
+            }}>
+Benefits
+            </div>
+
+
+<div>
+          <h1
+            style={{
+              fontSize: "clamp(2rem, 5vw, 2.8rem)",
+              color: "white",
+              lineHeight: "1.2",
+              margin: "0 0 20px 0",
+            }}
+          >
+            Benefits of Using AIS 140 GPS<br/> Tracking Devices
+          </h1>
+
+
+
+
+
+
+  <div class="m-3 grid grid-cols-2">
+    <div style={{ backgroundColor: "#44444480",  padding: "10px", borderRadius: "20px",
+      border: "1px solid #504e4e", display: "flex",  alignItems: "center", gap: "15px",  }} >
+       
+      <div style={{    backgroundColor: "#b60d0d4b", color: "#ff0000", padding: "6px 12px", fontSize: "13px",
+    borderRadius: "12px", fontWeight: "bold", border: "1px solid #ff0000",}}>❌</div>
+       <h4  style={{color:"white",fontSize:"20px"}}> Government Compliance Made Easy</h4><br/>
+       <p> Meets AIS 140 regulations required for approved vehicle categories.</p>
+    </div>
 
 
 
@@ -20,6 +64,27 @@ return (
 
 
 
+
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+</div>
 
 
 
